215192a9d34a389cd83c74ab7cdc447ab4ab3216
Fábio Miranda <fabioviannatj@gmail.com>
2019-05-13 21:05:21 -0300

    feat: Create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     HomeComponent
   ],
   imports: [
